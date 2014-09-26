f73700c5e3ab528cfc95acb80e9500e2194087b2
vivek <vchaure@northbounddgs.com>
2014-09-26 09:59:32 +0530

    class 'dropup' or 'dropdown' to change popupbehavior

--- src/angularjs-dropdown-multiselect.js
+++ src/angularjs-dropdown-multiselect.js
@@ -20,7 +20,7 @@ directiveModule.directive('ngDropdownMultiselect', ['$filter', '$document', '$co
                 var checkboxes = attrs.checkboxes ? true : false;
                 var groups = attrs.groupBy ? true : false;
 
-                var template = '<div class="multiselect-parent btn-group dropdown-multiselect">';
+                var template = '<div class="multiselect-parent btn-group dropdown-multiselect" ng-class="settings.dropdownVariation">';
                 template += '<button type="button" class="dropdown-toggle" ng-class="settings.buttonClasses" ng-click="toggleDropdown()">{{getButtonText()}}&nbsp;<span class="caret"></span></button>';
                 template += '<ul class="dropdown-menu dropdown-menu-form" ng-style="{display: open ? \'block\' : \'none\', height : settings.scrollable ? settings.scrollableHeight : \'auto\' }" style="overflow: scroll" >';
                 template += '<li ng-hide="!settings.showCheckAll || settings.selectionLimit > 0"><a data-ng-click="selectAll()"><span class="glyphicon glyphicon-ok"></span>  {{texts.checkAll}}</a>';
